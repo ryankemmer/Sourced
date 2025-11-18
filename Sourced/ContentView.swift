32457f8958991c29551e7e2414db7504f6c9aa47
Ryan Kemmer <ryankemmer77@gmail.com>
2025-11-17 21:05:48 -0500

    Initial Commit

---
+++ Sourced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sourced
+//
+//  Created by Ryan Kemmer on 11/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sourced/SourcedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourcedApp.swift
+//  Sourced
+//
+//  Created by Ryan Kemmer on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SourcedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
